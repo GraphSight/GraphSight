0a7d4f65763e07a5678c0e3d83e154e1379b598f
Trystan <c-trystan.bennett@charter.com>
2022-03-12 12:16:20 -0700

    fixed projects outside of solution directory

---
+++ GraphSight.SampleProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using GraphSight.Core.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GraphSight.SampleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GraphSightClient client = new GraphSightClient(
+                username: "", 
+                password: "", 
+                URI: "", 
+                secret: "");
+
+        }
+    }
+}
