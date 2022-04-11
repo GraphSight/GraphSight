e902b922f9d719751ccce4ad245f6494e783345e
Trystan <c-trystan.bennett@charter.com>
2022-04-10 23:23:03 -0600

    Started work on reflection tool to parse namespace for given attributes (intention is to try to build schema or pull data based on these)

---
+++ GraphSight.Core/Attributes/TestAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphSight.Core
+{
+    [AttributeUsage(AttributeTargets.All, Inherited = true, AllowMultiple = false)]
+    public class MyAttributeDefinition : Attribute
+    {
+        
+    }
+}
---
+++ GraphSight.Core/Reflection/NamespaceIterator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace GraphSight.Core
+{
+    public class NamespaceIterator
+    {
+        public static IEnumerable<Type> GetCallerNamespaceTypesContainingAttribute(Attribute attribute) 
+        {
+            //Get available namespace types
+            MethodBase methodInfo = new StackTrace().GetFrame(1).GetMethod();
+            Type[] types = methodInfo.Module.Assembly.GetTypes();
+
+            IEnumerable<Type> attributeTypes = types
+                .Where(type => Attribute.IsDefined(type, attribute.GetType()));
+
+            return attributeTypes; 
+        }
+    }
+}
